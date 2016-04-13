3fdf96f0bbcd6462a4769e6e650053a1008cacb8
army8735 <army8735@qq.com>
2016-04-13 17:08:08 +0800

    2.x remove lie

--- src/lefty.js
+++ src/lefty.js
@@ -1,10 +1,6 @@
 import homunculus from 'homunculus';
 import jsdc from 'jsdc';
 import Tree from './Tree';
-import lowIe from './lowIe';
-
-var Token = homunculus.getClass('token', 'jsx');
-var Node = homunculus.getClass('node', 'jsx');
 
 class Lefty {
   constructor() {
@@ -12,26 +8,11 @@ class Lefty {
     this.node = null;
   }
 
-  parse(code, lie, es5) {
+  parse(code, es5) {
     this.parser = homunculus.getParser('jsx');
     this.node = this.parser.parse(code);
     var tree = new Tree();
     var res = tree.parse(this.node);
-    if(lie) {
-      var parser = homunculus.getParser('es6');
-      var node = parser.parse(res);
-      var lexer = parser.lexer;
-      var ids = {};
-      lexer.tokens().forEach(function(token) {
-        if(token.type() == Token.ID) {
-          var s = token.content();
-          if(/^\d+$/.test(s)) {
-            ids['_' + s] = true;
-          }
-        }
-      });
-      res = lowIe.parse(node, ids);
-    }
     return es5 ? jsdc.parse(res) : res;
   }
 
--- web/lefty.js
+++ web/lefty.js
@@ -1,10 +1,6 @@
 define(function(require, exports, module){var homunculus=function(){var _0=require('homunculus');return _0.hasOwnProperty("default")?_0["default"]:_0}();
 var jsdc=function(){var _1=require('jsdc');return _1.hasOwnProperty("default")?_1["default"]:_1}();
 var Tree=function(){var _2=require('./Tree');return _2.hasOwnProperty("default")?_2["default"]:_2}();
-var lowIe=function(){var _3=require('./lowIe');return _3.hasOwnProperty("default")?_3["default"]:_3}();
-
-var Token = homunculus.getClass('token', 'jsx');
-var Node = homunculus.getClass('node', 'jsx');
 
 
   function Lefty() {
@@ -12,26 +8,11 @@ var Node = homunculus.getClass('node', 'jsx');
     this.node = null;
   }
 
-  Lefty.prototype.parse = function(code, lie, es5) {
+  Lefty.prototype.parse = function(code, es5) {
     this.parser = homunculus.getParser('jsx');
     this.node = this.parser.parse(code);
     var tree = new Tree();
     var res = tree.parse(this.node);
-    if(lie) {
-      var parser = homunculus.getParser('es6');
-      var node = parser.parse(res);
-      var lexer = parser.lexer;
-      var ids = {};
-      lexer.tokens().forEach(function(token) {
-        if(token.type() == Token.ID) {
-          var s = token.content();
-          if(/^\d+$/.test(s)) {
-            ids['_' + s] = true;
-          }
-        }
-      });
-      res = lowIe.parse(node, ids);
-    }
     return es5 ? jsdc.parse(res) : res;
   }
 
